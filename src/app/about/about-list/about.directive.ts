d4544a6a06601b99eeeb0508a0e0d00a327e5c20
徐登伟 <1103579820@qq.com>
2018-06-09 15:54:16 +0800

    add hostListener for directive

--- src/app/about/about-list/about.directive.ts
+++ src/app/about/about-list/about.directive.ts
@@ -1,17 +1,26 @@
 
-import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Input, HostListener } from '@angular/core';
 
 @Directive({
     selector: '[heighLight]'
 })
 export class AboutDirective implements AfterViewInit{
-    constructor(el: ElementRef){
-        // el.nativeElement.style. = 'red';
-        el.nativeElement.style.backgroundColor = 'yellow';
+    constructor(private el: ElementRef){}
+
+    @HostListener('mouseenter') onMouseEnter() {
+        this.highlight('yellow');
+    }
+
+    @HostListener('mouseleave') onMouseLeave() {
+        this.highlight(null);
     }
 
 
     ngAfterViewInit() {
         console.error('执行到了指令。。。。');
+    }
+
+    private highlight(color: string) {
+        this.el.nativeElement.style.backgroundColor = color;
     }
 }
